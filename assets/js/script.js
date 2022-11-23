bdc0b5fd1b34bd716d848c6a7709370995ffc8a7
rabia-desing <hassan.uarid@gmail.com>
2022-11-23 16:55:55 +0500

    api changed added some more countries to list

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function getCountry() {
+    let endpoint = "http://ip-api.com/json/?fields=country"; //get user location form this api.
+
+    fetch(endpoint).then(
+        function (response) {
+            let promiseForward = response.json();
+            return promiseForward;
+        }
+    ).then(
+        function (data) {
+            console.log(data.country);
+            let countryName = document.getElementById("countryName");
+            countryName.innerHTML = data.country;
+        }
+    ).catch(
+        function (error) {
+            console.log(error)
+        });
+}
+
+getCountry();
+
+function getCurrencyRates() {
+    fetch("https://api.exchangerate.host/latest?base=USD&symbols=AED,GBP,EUR")
+        .then(
+            function(response){ return (response.json())}
+            )
+        .then(
+            function (result) {
+                document.getElementById("aed").innerHTML=result.rates.AED;
+                document.getElementById("gbp").innerHTML=result.rates.GBP;
+                document.getElementById("eur").innerHTML=result.rates.EUR;
+            }
+            )
+        .catch(function (error) { 
+            console.log('error', error)
+        })
+}
+getCurrencyRates();
